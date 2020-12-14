aa2a1aa7125aa0ff712e8045ce6e3e82e17ff3f3
0cramium <Marcoalagna1@gmail.com>
2020-12-13 23:07:15 -0600

    Basic inventory :)

--- Source-Code/paradox-functs.cpp
+++
@@ -1,9 +0,0 @@
-#include <iostream>
-#include <string>
-#include <stdlib.h>
-#include <chrono>
-#include <thread>
-
-void sSlp(int msec) {
-    std::this_thread::sleep_for(std::chrono::milliseconds(msec));
-}
---
+++ src/px.h
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <string>
+#include <stdlib.h>
+#include <vector>
+
+std::string tool;
+
+int temp;
+int choise;
+int crclass;
+
+bool partner = false;
+bool isDead;
+
+void rInv();
+void sSlp(int);
